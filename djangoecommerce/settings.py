855e61fd993d417c1be8e68d18a0a3c372b32c81
Lucier Ferreira Lima <lucierflima@gmail.com>
2018-08-18 01:35:25 -0300

    adquação banco

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -142,6 +142,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 # E-mail
 EMAIL_HOST = ''
 EMAIL_HOST_USER = ''
